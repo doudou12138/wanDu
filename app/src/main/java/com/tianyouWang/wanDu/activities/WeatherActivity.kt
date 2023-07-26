9812427bbcf931338b9edf6e3f22620bbc97d7e5
doudou <211250173@smail.nju.edu.cn>
2023-07-26 23:23:26 +0800

    ProjectV1.4 UIV1.4 完善了天气页面的UI，但是使用的还是假数据，固定的死的

--- app/src/main/java/com/tianyouWang/wanDu/activities/WeatherActivity.kt
+++ app/src/main/java/com/tianyouWang/wanDu/activities/WeatherActivity.kt
@@ -4,10 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.Alignment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tianyouWang.wanDu.R
+import com.tianyouWang.wanDu.adapter.WeatherAdapter
+import com.tianyouWang.wanDu.bean.WeatherItemBean
+import com.tianyouWang.wanDu.enum.WeatherType
 
 class WeatherActivity :ComponentActivity() {
+    private var weatherRecycleView: RecyclerView? = null
+    private var weatherAdapter: WeatherAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
@@ -17,5 +27,25 @@ class WeatherActivity :ComponentActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+
+        addRecycleView();
+    }
+    
+    private fun addRecycleView(){
+        weatherRecycleView = findViewById(R.id.weather_list)
+        weatherAdapter = WeatherAdapter(onCreateWeatherData())
+        weatherRecycleView?.adapter = weatherAdapter
+        weatherRecycleView?.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+
+    }
+
+    private fun onCreateWeatherData() :List<WeatherItemBean>{
+        val result = ArrayList<WeatherItemBean>();
+        for( i in 11..20){
+            val bean = WeatherItemBean(i.toString()+"时",WeatherType.SUNNY)
+            result.add(bean)
+        }
+
+        return result
     }
 }
