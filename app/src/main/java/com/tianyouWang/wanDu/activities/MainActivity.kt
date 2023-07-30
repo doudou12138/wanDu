39f69ee02cfd5f4a790acf7f3fdebec2112572d9
doudou <211250173@smail.nju.edu.cn>
2023-07-30 13:56:03 +0800

    ProjectV1.5.1 完成了多tabs demo UIV1.5.1 多tabs，demo LogicV1.5.1

--- app/src/main/java/com/tianyouWang/wanDu/activities/MainActivity.kt
+++ app/src/main/java/com/tianyouWang/wanDu/activities/MainActivity.kt
@@ -2,11 +2,16 @@ package com.tianyouWang.wanDu.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tianyouWang.wanDu.R
@@ -14,10 +19,14 @@ import com.tianyouWang.wanDu.adapter.NewsAdapter
 import com.tianyouWang.wanDu.bean.NewsItemBean
 import com.tianyouWang.wanDu.bean.NewsItemBeanWithAuther
 import com.tianyouWang.wanDu.bean.NewsItemBeanWithImage
+import com.tianyouWang.wanDu.fragments.Fragment1
+import com.tianyouWang.wanDu.fragments.Fragment2
+import com.tianyouWang.wanDu.fragments.Fragment3
 import util.LoadingUtils
+import kotlin.math.log
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity(){
 
     /** RecycleView 实例 */
     private var newsRecycleView: RecyclerView? = null
@@ -25,23 +34,29 @@ class MainActivity : ComponentActivity() {
     /** RecycleView 的适配器 */
     private var newsAdapter: NewsAdapter? = null
 
+    /** 进行fragments之间跳转的管理 **/
+    private var fragmentTransaction: FragmentTransaction? = null
+
+    /** 记录现在处于哪个fragment **/
+    private var now_page: Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        val weatherPart = findViewById<LinearLayout>(R.id.weather)
-        weatherPart.setOnClickListener {
-            // 在这里处理点击事件
-            // 执行跳转到指定页面的操作
-            LoadingUtils.startWithLoading(this,WeatherActivity::class.java)
+        setContentView(R.layout.tabs_demo)
+        //val weatherPart = findViewById<LinearLayout>(R.id.weather)
+//        weatherPart.setOnClickListener {
+//            // 在这里处理点击事件
+//            // 执行跳转到指定页面的操作
+//            LoadingUtils.startWithLoading(this,WeatherActivity::class.java)
+//
+//        }
 
-        }
-
-        var wanDuOnce = findViewById<TextView>(R.id.searchNow)
-
-        wanDuOnce.setOnClickListener{
-            Toast.makeText(this,
-                "正在努力检索了，请小主稍等", Toast.LENGTH_SHORT).show()
-        }
+//        var wanDuOnce = findViewById<TextView>(R.id.searchNow)
+//
+//        wanDuOnce.setOnClickListener{
+//            Toast.makeText(this,
+//                "正在努力检索了，请小主稍等", Toast.LENGTH_SHORT).show()
+//        }
 
 //        var newsClick = findViewById<LinearLayout>(R.id.news_list0)
 //        newsClick.setOnClickListener{
@@ -59,8 +74,10 @@ class MainActivity : ComponentActivity() {
 //                }
 //            }
 //        }
-        addRecycleView();
-        weather_alpha();
+        //addRecycleView();
+        //weather_alpha();
+        addFragment()
+        addFragmentsSwitch()
     }
 
     private fun addRecycleView(){
@@ -70,6 +87,45 @@ class MainActivity : ComponentActivity() {
         newsRecycleView?.layoutManager = LinearLayoutManager(this)
     }
 
+    private fun addFragment() {
+        val fragment1: Fragment = Fragment1()
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction!!.add(R.id.content, fragment1)
+        fragmentTransaction!!.commit()
+        now_page=0;
+    }
+
+    private fun setSelectPage(page_no :Int){
+        if(now_page==page_no){
+
+        }else{
+            now_page = page_no
+            val fragmentManager : FragmentManager = supportFragmentManager
+            fragmentTransaction = fragmentManager.beginTransaction()
+
+            fragmentTransaction?.replace(R.id.content,getFragmentByPageNo(page_no))?.commit()
+
+        }
+    }
+
+    private fun addFragmentsSwitch(){
+        val homePageButton = findViewById<TextView>(R.id.homePageButton)
+        homePageButton.setOnClickListener {
+            setSelectPage(0)
+        }
+        val video = findViewById<TextView>(R.id.videoPageButton)
+        video.setOnClickListener{
+            setSelectPage(1)
+        }
+
+        val myPageText = findViewById<TextView>(R.id.myPageButton)
+        myPageText.setOnClickListener{
+            setSelectPage(2)
+
+        }
+    }
     /*
     启动天气的透明度动画
      */
@@ -102,6 +158,15 @@ class MainActivity : ComponentActivity() {
         result.add(first_item_auther)
 
         return result
+    }
+
+    private fun getFragmentByPageNo(page_no: Int): Fragment {
+        return when (page_no) {
+            0 -> Fragment1()
+            1 -> Fragment2()
+            2 -> Fragment3()
+            else -> throw IllegalArgumentException("Invalid page number: $page_no")
+        }
     }
 
 }
