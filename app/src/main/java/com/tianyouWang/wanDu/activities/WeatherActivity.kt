dcfa6be3f51741fa099324d75237c6614e16a762
doudou <211250173@smail.nju.edu.cn>
2023-07-31 22:11:22 +0800

    ProjectV1.6.4 调整了ui，添加了部分注释 UIV1.6.4 调整了fragment3的ui，个人信息的假数据，包括头像，网名，描述的初始值;调整了天气的假数据 Logic1.6.4  给天气页面添加了检查警告，并修改可见性的盒子

--- app/src/main/java/com/tianyouWang/wanDu/activities/WeatherActivity.kt
+++ app/src/main/java/com/tianyouWang/wanDu/activities/WeatherActivity.kt
@@ -3,10 +3,12 @@ package com.tianyouWang.wanDu.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.Alignment
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tianyouWang.wanDu.R
@@ -15,6 +17,19 @@ import com.tianyouWang.wanDu.bean.WeatherItemBean
 import com.tianyouWang.wanDu.enum.WeatherType
 
 class WeatherActivity :ComponentActivity() {
+    private val weather_each_hour: List<WeatherType> = listOf(
+        WeatherType.SUNNY,
+        WeatherType.SUNNY,
+        WeatherType.CLOUDY,
+        WeatherType.CLOUDY,
+        WeatherType.CLOUDY,
+        WeatherType.CLOUDY,
+        WeatherType.SUNNY,
+        WeatherType.CLOUDY,
+        WeatherType.RAINY,
+        WeatherType.RAINY
+    )
+
     private var weatherRecycleView: RecyclerView? = null
     private var weatherAdapter: WeatherAdapter? = null
 
@@ -29,8 +44,10 @@ class WeatherActivity :ComponentActivity() {
         }
 
         addRecycleView();
+        checkWarn()
     }
-    
+
+    //加入横向的小时天气RecycleView
     private fun addRecycleView(){
         weatherRecycleView = findViewById(R.id.weather_list)
         weatherAdapter = WeatherAdapter(onCreateWeatherData())
@@ -39,13 +56,28 @@ class WeatherActivity :ComponentActivity() {
 
     }
 
+    //制造小时天气的假数据
     private fun onCreateWeatherData() :List<WeatherItemBean>{
         val result = ArrayList<WeatherItemBean>();
         for( i in 11..20){
-            val bean = WeatherItemBean(i.toString()+"时",WeatherType.SUNNY)
+            val bean = WeatherItemBean(i.toString()+"时",weather_each_hour.get(i-11))
             result.add(bean)
         }
 
         return result
+    }
+
+    /*
+    检查是否有恶劣天气引发警告，从而调整页面中warn的可见性
+     */
+    private fun checkWarn(){
+        val warn = findViewById<ConstraintLayout>(R.id.warn)
+        val hasWarn = false
+        if(hasWarn){
+            //
+            warn.visibility = View.VISIBLE
+        }else{
+            warn.visibility = View.INVISIBLE
+        }
     }
 }
